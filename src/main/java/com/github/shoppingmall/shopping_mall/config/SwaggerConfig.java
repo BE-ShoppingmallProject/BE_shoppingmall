e94505868d36aa27c7adc259c43feeebdf7213f6
peten1105 <peten@kakao.com>
2023-12-26 01:58:20 +0900

    Feature Sell
    - seller와 관련된 Service, Controller, Entity, Repository 추가

---
+++ src/main/java/com/github/shoppingmall/shopping_mall/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.github.shoppingmall.shopping_mall.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("쇼핑몰 프로젝트 API")
+                        .description("설명 수정중..")
+                        .version("1.0.0"));
+    }
+}
---
+++ src/main/java/com/github/shoppingmall/shopping_mall/repository/SellerItem/SellerItemRequest.java
@@ -0,0 +1,4 @@
+package com.github.shoppingmall.shopping_mall.repository.SellerItem;
+
+public class SellerItemRequest {
+}
